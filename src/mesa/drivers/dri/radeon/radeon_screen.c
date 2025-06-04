3ec131fbaca7b556e06918397c90f6df0e26c607
Dave Milici <milici.dave@gmail.com>
2025-06-04 14:57:02 -0700

    Port stereo swap mode option into Radeon drivers.

--- src/mesa/drivers/dri/radeon/radeon_screen.c
+++ src/mesa/drivers/dri/radeon/radeon_screen.c
@@ -824,7 +824,7 @@ __DRIconfig **radeonInitScreen2(__DRIscreen *psp)
    };
 
    static const GLenum back_buffer_modes[] = {
-      __DRI_ATTRIB_SWAP_NONE, __DRI_ATTRIB_SWAP_UNDEFINED
+      __DRI_ATTRIB_SWAP_NONE, __DRI_ATTRIB_SWAP_UNDEFINED, __DRI_ATTRIB_STEREO
    };
    uint8_t depth_bits[4], stencil_bits[4], msaa_samples_array[1];
    int color;
